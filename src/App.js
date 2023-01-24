ce5d401feb9419e015bab9955351f1e7676b1dd9
JamilAmasa <jamilamasa@gmail.com>
2023-01-24 13:10:19 +0100

    add expense

--- src/App.js
+++ src/App.js
@@ -1,49 +1,34 @@
 import { useState } from "react";
 import "./App.css";
 import Controls from "./components/Controls";
-import Modal from "./components/Modal/Modal";
+import Modal from "./components/Modal";
 import NavBar from "./components/NavBar";
 import NetOutput from "./components/NetOutput";
 import Table from "./components/Table";
 
 function App() {
   const [modal, setModal] = useState({ status: false, type: "" });
+  const [expenses, setExpense] = useState([]);
+  // Set modal visibility
   const showModal = (modalType) => {
     setModal({ status: true, type: modalType });
   };
-  const hideModal = ()=> {
-    setModal({ status: false, type: "" })
-  }
-  const expenses = [
-    {
-      id: 1,
-      date: "30/20/23",
-      description: "chow",
-      amount: 3000,
-      transaction: "income",
-    },
-    {
-      id: 2,
-      date: "30/20/23",
-      description: "chow",
-      amount: 2000,
-      transaction: "expense",
-    },
-    {
-      id: 3,
-      date: "30/20/23",
-      description: "chow",
-      amount: 2000,
-      transaction: "income",
-    },
-  ];
+  const hideModal = () => {
+    setModal({ status: false, type: "" });
+  };
+
+  // Add expense to DS
+  const addExpenseHandler = (newExpense) => {
+    setExpense((prevExpense) => [newExpense, ...prevExpense]);
+  };
+  console.log(expenses)
   return (
     <div>
       <NavBar />
       <div className="body p-3 overflow-hidden">
         <div className="row gx-3">
           <div className="col flex1" id="controls">
-            <Controls onShowModal={showModal}/>
+            <Controls onShowModal={showModal} />
           </div>
           <div className="col flex3 vh-89 z-index-n2 overflow-auto">
             <Table expenses={expenses} />
@@ -53,9 +38,10 @@ function App() {
           </div>
         </div>
       </div>
-      {modal.status === true && (<Modal type={modal.type} onHideModal={hideModal}/>)}
+      {modal.status === true && (
+        <Modal type={modal.type} onAddExpense={addExpenseHandler} onHideModal={hideModal} />
+      )}
     </div>
- 
   );
 }
 
---
+++ src/components/Modal.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+
+const Modal = (props) => {
+  const [inputData, setInputData] = useState({
+    date: "",
+    description: "",
+    amount: '',
+  });
+  const inputDateHandler = (e) => {
+    setInputData((prevData) => {
+      return { ...prevData, date: e.target.value };
+    });
+  };
+  const inputDescriptionHandler = (e) => {
+    setInputData((prevData) => {
+      return { ...prevData, description: e.target.value };
+    });
+  };
+  const inputAmountHandler = (e) => {
+    setInputData((prevData) => {
+      return { ...prevData, amount: e.target.value };
+    });
+  };
+  const addExpenseHandler = () => {
+    props.onAddExpense(inputData)
+  }
+  const hideModalHandler = () => {
+    props.onHideModal();
+  };
+
+  return (
+    <div className="card mt-3 position-absolute t-40 start-50 translate-middle w-50 m-auto">
+      <div className="card-header display-6 fw-semibold">Add {props.type}</div>
+      <div className="card-body">
+        <form>
+          <div className="form-floating mb-3">
+            <input
+              onChange={inputDateHandler}
+              value={inputData.date}
+              type="date"
+              className="form-control"
+              id="date"
+              placeholder="dd/mm/yyyy"
+            ></input>
+            <label htmlFor="date">Date</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              onChange={inputDescriptionHandler}
+              value={inputData.description}
+              type="text"
+              className="form-control"
+              id="description"
+              placeholder="Food"
+            ></input>
+            <label htmlFor="description">Description</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              onChange={inputAmountHandler}
+              value={inputData.amount}
+              type="number"
+              className="form-control"
+              id="amount"
+              placeholder="20000"
+            ></input>
+            <label htmlFor="amount">Amount</label>
+          </div>
+        </form>
+      </div>
+      <div className="card-footer">
+        <div className="btn-group float-end btn-group-lg" role="group">
+          <button
+            type="button"
+            onClick={hideModalHandler}
+            className="btn btn-secondary"
+          >
+            Close
+          </button>
+          <button type="button" onClick={addExpenseHandler} className="btn btn-primary">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
