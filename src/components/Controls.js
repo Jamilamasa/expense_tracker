63b84788a281fd304bbf6977e5756eeec556c058
JamilAmasa <jamilamasa@gmail.com>
2023-01-26 13:48:14 +0100

    Fareed

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -2,45 +2,32 @@ import React from "react";
 
 export default function Controls(props) {
   const showExpenseModalHandler = () => {
-    props.onShowModal("expense")
-  }
+    props.onShowModal("expense");
+  };
   const showIncomeModalHandler = () => {
-    props.onShowModal("income")
-  }
+    props.onShowModal("income");
+  };
 
-  const { expenses } = props
+  const { expenses } = props;
 
-  let totalIncome = 0
-  let totalExpense = 0
+  let totalIncome = 0;
+  let totalExpense = 0;
   const allIncome = expenses.filter((expense) => {
-
-    return expense.transaction ===
-      'income'
-
-  })
+    return expense.transaction === "income";
+  });
   const allExpense = expenses.filter((expense) => {
-
-    return expense.transaction ===
-      'expense'
-
-  })
+    return expense.transaction === "expense";
+  });
 
   for (let i = 0; i < allIncome.length; i++) {
-    totalIncome += parseInt(allIncome[i].amount)
+    totalIncome += parseInt(allIncome[i].amount);
   }
   for (let i = 0; i < allExpense.length; i++) {
-    totalExpense += parseInt(allExpense[i].amount)
+    totalExpense += parseInt(allExpense[i].amount);
   }
 
-  console.log(parseInt(totalIncome));
-
-
-
-
-
-  let profit = totalIncome - totalExpense
-  let percentProfit = (profit * 100) / totalIncome
-
+  let profit = totalIncome - totalExpense;
+  let percentProfit = (profit * 100) / totalIncome;
 
   return (
     <div>
@@ -55,10 +42,20 @@ export default function Controls(props) {
         </div>
       </div>
       <div className="container mt-3">
-        <button type="button" className="btn btn-danger m-0" id="addExpense" onClick={showExpenseModalHandler}>
+        <button
+          type="button"
+          className="btn btn-danger m-0"
+          id="addExpense"
+          onClick={showExpenseModalHandler}
+        >
           Add Expense
         </button>
-        <button type="button" className="btn btn-success" id="addIncome" onClick={showIncomeModalHandler}>
+        <button
+          type="button"
+          className="btn btn-success"
+          id="addIncome"
+          onClick={showIncomeModalHandler}
+        >
           Add Income
         </button>
       </div>
