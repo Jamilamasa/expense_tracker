7801351015edd4d82d6d0810865ed21b2f0663fd
spyatmycode <akejunifemi11@gmail.com>
2023-01-26 12:35:43 +0100

    Minor fixes: Controls.js

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -7,15 +7,48 @@ export default function Controls(props) {
   const showIncomeModalHandler = () => {
     props.onShowModal("income")
   }
+
+  const { expenses } = props
+
+  let totalIncome = 0
+  let totalExpense = 0
+  const allIncome = expenses.filter((expense) => {
+
+    return expense.transaction ===
+      'income'
+
+  })
+  const allExpense = expenses.filter((expense) => {
+
+    return expense.transaction ===
+      'expense'
+
+  })
+
+  for (let i = 0; i < allIncome.length; i++) {
+    totalIncome += parseInt(allIncome[i].amount)
+  }
+  for (let i = 0; i < allExpense.length; i++) {
+    totalExpense += parseInt(allExpense[i].amount)
+  }
+
+  console.log(parseInt(totalIncome));
+
+
+
+  let profit = totalIncome - totalExpense
+  let percentProfit = (profit * 100) / totalIncome
+
+
   return (
     <div>
       <div className="card">
         <div className="card-body">
           <h5>
-            Total Expense: <span id="totalExpense">0</span>
+            Total Expense: <span id="totalExpense">&#8358;{totalExpense}</span>
           </h5>
           <h5>
-            Total Income: <span id="totalIncome">0</span>
+            Total Income: <span id="totalIncome">&#8358;{totalIncome}</span>
           </h5>
         </div>
       </div>
